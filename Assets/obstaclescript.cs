1ed7ce70401fe2526196faa6d71a27a3755d9e2f
Matias Alexander Rømer <mati0681@edu.easj.dk>
2018-05-28 15:13:49 +0200

    Pathfinding

---
+++ Assets/obstaclescript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstaclescript : MonoBehaviour
+{
+
+    public float styrke = 4f;
+    public float fart = 2f;
+
+    private float randomOs;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    randomOs = Random.Range(0f, 2f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 pos = transform.position;
+	    pos.x = Mathf.Sin(Time.time * fart + randomOs) * styrke;
+	    transform.position = pos;
+	}
+}
